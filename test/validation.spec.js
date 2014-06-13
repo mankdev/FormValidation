702f9ad3d2e71ea69943056aacd11eb2766b0b26
mankdev <tema@mankdev.name>
2014-06-14 02:29:12 +0400

    revert

--- test/validation.spec.js
+++ test/validation.spec.js
@@ -67,7 +67,7 @@ describe('Validation', function() {
 			var helper = new FormHelper(formsTpl, 'novalidatableForm'),
 				validation = new FormValidation(helper.form);
 
-			expect(validation.checkValidity()).toBe(false);
+			expect(validation.checkValidity()).toBe(true);
 		});
 
 		it('should fire E_FORM_VALID if form valid', function(done) {
